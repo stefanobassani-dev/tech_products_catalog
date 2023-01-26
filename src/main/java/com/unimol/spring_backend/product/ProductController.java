cb798c4f094e63d64cd0d182c53ba3c6a8c55701
stefanobassani-dev <stefanobassani.dev@gmail.com>
2023-01-27 00:32:36 +0100

    added checkstyle suppressions and modified cors config file

---
+++ src/main/java/com/unimol/spring_backend/product/ProductController.java
@@ -0,0 +1,24 @@
+package com.unimol.spring_backend.product;
+
+import com.unimol.spring_backend.product.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/products")
+@RequiredArgsConstructor
+public class ProductController {
+    private final ProductService productService;
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getAllProductsHandler() {
+        this.productService.insertAllProducts();
+
+        return ResponseEntity.ok(this.productService.getAllProducts());
+    }
+}
